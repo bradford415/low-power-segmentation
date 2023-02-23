603e5361cf75c6babbaaee75ecd61f7e7e1c7004
Bradley Selee <34605638+bradford415@users.noreply.github.com>
2023-02-23 12:24:07 -0500

    Delete inference.sh

--- inference.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-python main.py --experiment bn_lr7e-3 \
-               --backbone resnet101 \
-               --dataset pascal \
-               --epochs 50 \
-               --batch_size 4 \
-               --base_lr 0.007 \
