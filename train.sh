a496a1fed37b2451d052682336c546464d8505c0
Bradley Selee <34605638+bradford415@users.noreply.github.com>
2023-02-23 12:24:13 -0500

    Delete train.sh

--- train.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-python main.py --train \
-               --experiment bn_lr7e-3 \
-               --backbone resnet101 \
-               --dataset pascal \
-               --base_lr 0.007 \
-                
