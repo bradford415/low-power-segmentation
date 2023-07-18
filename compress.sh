e4ed026f6c168d4dda2b63495e34290d11cffa9c
Bradley Selee <bselee415@gmail.com>
2023-07-18 13:04:27 -0400

    comment compress.sh

--- compress.sh
+++ compress.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Must zip with 3.6 interpreter or else the competition will consider it invalid
 $(python3.6 -m zipapp -p "/usr/bin/env python3.6" solution)
 
 $(mv solution.pyz  evaluation/)
